46860efcab1257a9f7247fcbc271eeaf7deed484
alvinqid <github@alvinq.my.id>
2026-03-13 16:30:05 +0700

    fix or test!

--- app/src/main/java/org/levimc/launcher/core/mods/ModManager.java
+++ app/src/main/java/org/levimc/launcher/core/mods/ModManager.java
@@ -11,6 +11,7 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Objects;
 import org.levimc.launcher.util.LLModBuilder;
